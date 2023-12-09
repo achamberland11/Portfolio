204f6c8800eb1c564b87656b9652e13f19244dd2
Virgile <a.chamberland11@gmail.com>
2023-12-08 19:19:07 -0500

    Génération automatique des projets sur la page d'accueil

---
+++ js/projets.js
@@ -0,0 +1,44 @@
+const donneesProjets = [
+    {
+        titre: "Chroniques Dystopiques",
+        technologie: "Unity 3D - C#",
+        date: "2023 - Projet en Équipe (4)",
+        lien: "index-projets/chroniques-dystopiques.html",
+        id: "c-d",
+    },
+    {
+        titre: "Transit",
+        technologie: "Unity 3D - C#",
+        date: "2022",
+        lien: "index-projets/transit.html",
+        id: "transit",
+    },
+    {
+        titre: "Myosotis Memories",
+        technologie: "Unity 2D - C# (Gamejam)",
+        date: "2023 - Projet en Équipe (4)",
+        lien: "index-projets/myositis.html",
+        id: "MM",
+    },
+    {
+        titre: "Chapeau Spree",
+        technologie: "Unity 3D - C# (Multijoueur)",
+        date: "2023 - Projet en Équipe (2)",
+        lien: "index-projets/chapeau-spree.html",
+        id: "chapeau",
+    },
+    {
+        titre: "L'aventure de la Moustache",
+        technologie: "Unity 2D - C#",
+        date: "2022",
+        lien: "index-projets/moustache.html",
+        id: "moustache",
+    },
+    {
+        titre: "Bananarama",
+        technologie: "JavaScript - HTML",
+        date: "2021",
+        lien: "index-projets/bananarama.html",
+        id: "bananarama",
+    },
+];
