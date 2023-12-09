d05e303913f1fa014433339cc61307c1c9bea4b6
Virgile <a.chamberland11@gmail.com>
2023-12-09 00:00:04 -0500

    Revert "Modification des lien des vignette de l'accueil"
    This reverts commit 1ed293748679f497a139abdbf483480c6c941836.

--- js/projets.js
+++ js/projets.js
@@ -6,7 +6,6 @@ const donneesProjets = [
         lien: "index-projets/chroniques-dystopiques.html",
         id: "c-d",
         urlVignette: "url('../fichiers/images/CD.jpg');",
-        urlDirectVignette: "url('/fichiers/images/CD.jpg');",
     },
     {
         titre: "Transit",
@@ -15,7 +14,6 @@ const donneesProjets = [
         lien: "index-projets/transit.html",
         id: "transit",
         urlVignette: "url('../fichiers/images/transit.png');",
-        urlDirectVignette: "url('/fichiers/images/transit.png');",
     },
     {
         titre: "Myosotis Memories",
@@ -24,7 +22,6 @@ const donneesProjets = [
         lien: "index-projets/myositis.html",
         id: "MM",
         urlVignette: "url('../fichiers/images/myositis.png');",
-        urlDirectVignette: "url('/fichiers/images/myositis.png');",
     },
     {
         titre: "Chapeau Spree",
@@ -33,7 +30,6 @@ const donneesProjets = [
         lien: "index-projets/chapeau-spree.html",
         id: "chapeau",
         urlVignette: "url('../fichiers/images/chapeau.png');",
-        urlDirectVignette: "url('/fichiers/images/chapeau.png');",
     },
     {
         titre: "L'aventure de la Moustache",
@@ -42,7 +38,8 @@ const donneesProjets = [
         lien: "index-projets/moustache.html",
         id: "moustache",
         urlVignette: "url('../fichiers/images/moustache.png');",
-        urlDirectVignette: "url('/fichiers/images/moustache.png');",
+        type: "Jeu Unity 2D",
+        description: "",
     },
     {
         titre: "Bananarama",
@@ -51,6 +48,5 @@ const donneesProjets = [
         lien: "index-projets/bananarama.html",
         id: "bananarama",
         urlVignette: "url('../fichiers/images/bananarama-couverture.png');",
-        urlDirectVignette: "url('/fichiers/images/bananarama-couverture.png');",
     },
 ];
